4122d43f9b921d9b78abc178b0eb2780a8cdeefa
technovadev24 <novanovytska@gmail.com>
2025-03-31 03:43:11 +0200

    feat: successfully connected wallet with RainbowKit and MetaMask

--- frontend/components/shared/StockToChain.jsx
+++ frontend/components/shared/StockToChain.jsx
@@ -2,3 +2,11 @@
 import { useState, useEffect } from "react";
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "@/constants";
 import { useReadContract, useWatchContractEvent, useWaitForTransactionReceipt, useAccount } from "wagmi";
+
+const StockToChain = () => {
+    return (
+        <div>StockToChain</div>
+    )
+}
+
+export default StockToChain
